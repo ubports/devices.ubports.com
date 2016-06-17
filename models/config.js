a1434f24a85bbdebd93f383459c36fb1ac5ecf3e
Marius Gripsgard <mariogrip@ubuntu.com>
2016-06-17 02:36:42 +0200

    fix dependencies and fix configs

--- models/config.js
+++ models/config.js
@@ -1,6 +1,10 @@
-var env       = process.env.NODE_ENV || 'development';
+var env       = process.env.NODE_ENV || 'production';
 var config    = require(__dirname + '/../config/config.json')[env];
 
 config.DEBUG = process.env.DEBUG;
+
+if (config.DEBUG){
+    env = "development"
+}
 
 module.exports = config;
